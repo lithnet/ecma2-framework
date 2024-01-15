0543e07b91035ccc6e2723c22e22cadbef9bb6bf
Ryan Newington <ryan@lithiumblue.com>
2024-01-16 08:18:22 +1100

    Adds roslyn debugger component

---
+++ src/Lithnet.Ecma2Framework.SourceGenDebugger/CapabilitiesProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.MetadirectoryServices;
+
+namespace Lithnet.Ecma2Framework.SourceGenDebugger
+{
+    internal class CapabilitiesProvider : ICapabilitiesProvider
+    {
+        public Task<MACapabilities> GetCapabilitiesExAsync(KeyedCollection<string, ConfigParameter> configParameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Lithnet.Ecma2Framework.SourceGenDebugger/Ecma2Initializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Lithnet.Ecma2Framework.SourceGenDebugger
+{
+    internal class Ecma2Initializer : IEcmaBootstrapper
+    {
+        public string EventLogName { get; } = "Application";
+
+        public string EventLogSource { get; } = "Ecma2Framework";
+
+        public string ManagementAgentName { get; } = "SourceGenDebugger";
+
+        public void SetupServices(IServiceCollection services)
+        {
+        }
+    }
+}
