6fdc2a5fb92ac568a9ac5cd9955bd27f2bfa8dbe
Ryan Newington <ryan@lithiumblue.com>
2024-01-16 16:34:23 +1100

    Reorganize project into separate source generator and ecma2 implementation

---
+++ src/Lithnet.Ecma2Framework.SourceGeneration/EcmaGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Lithnet.Ecma2Framework
+{
+    [Generator]
+    public class EcmaGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new Ecma2InitializerSyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.SyntaxContextReceiver is not Ecma2InitializerSyntaxReceiver receiver)
+            {
+                return;
+            }
+
+            bool hasErrors = false;
+
+            if (!receiver.HasBootstrapper || string.IsNullOrWhiteSpace(receiver.BootstrapperClassName))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("ECMA2001", "Could not find the bootstrapper implementation", $"A bootstrapper class could not be found. Please generate a class that implements from IEcmaBootstrapper", "Ecma2Framework", DiagnosticSeverity.Error, true), Location.None));
+                hasErrors = true;
+            }
+
+            if (!receiver.HasSchemaProvider)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("ECMA2002", "Could not find the schema provider implementation", $"A schema provider class could not be found. Please generate a class that implements from ISchemaProvider", "Ecma2Framework", DiagnosticSeverity.Error, true), Location.None));
+                hasErrors = true;
+            }
+
+            if (!receiver.HasCapabilityProvider)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("ECMA2003", "Could not find the capabilities provider implementation", $"A capabilities provider class could not be found. Please generate a class that implements from ICapabilitiesProvider", "Ecma2Framework", DiagnosticSeverity.Error, true), Location.None));
+                hasErrors = true;
+            }
+
+            if (!receiver.HasObjectExportProvider && !receiver.HasObjectImportProvider)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("ECMA2004", "Could not find the object import or export provider implementation", $"An object provider class could not be found. Please generate at least one class that implements from IObjectExportProvider or IObjectImportProvider", "Ecma2Framework", DiagnosticSeverity.Error, true), Location.None));
+                hasErrors = true;
+            }
+
+            if (hasErrors)
+            {
+                return;
+            }
+
+            var bootstrapperText = this.GetResource("Lithnet.Ecma2Framework.SourceGeneration.Templates.Ecma2Bootstrapper.txt").Replace("%CLASSNAME%", receiver.BootstrapperClassName);
+            context.AddSource("Ecma2Bootstrapper.g.cs", SourceText.From(bootstrapperText, Encoding.UTF8));
+            context.AddSource("Ecma2ImportImplementation.g.cs", SourceText.From(this.GetResource("Lithnet.Ecma2Framework.SourceGeneration.Templates.Ecma2ImportImplementation.txt"), Encoding.UTF8));
+            context.AddSource("Ecma2ExportImplementation.g.cs", SourceText.From(this.GetResource("Lithnet.Ecma2Framework.SourceGeneration.Templates.Ecma2ExportImplementation.txt"), Encoding.UTF8));
+            context.AddSource("Ecma2PasswordImplementation.g.cs", SourceText.From(this.GetResource("Lithnet.Ecma2Framework.SourceGeneration.Templates.Ecma2PasswordImplementation.txt"), Encoding.UTF8));
+            context.AddSource("Ecma2Implementation.g.cs", SourceText.From(this.GetResource("Lithnet.Ecma2Framework.SourceGeneration.Templates.Ecma2Implementation.txt"), Encoding.UTF8));
+        }
+
+        private string GetResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- src/Lithnet.Ecma2Framework/GenericExtensions.cs
+++ src/Lithnet.Ecma2Framework/GenericExtensions.cs
@@ -1,29 +1,11 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Security;
-using Microsoft.CodeAnalysis;
 
 namespace Lithnet.Ecma2Framework
 {
     internal static class GenericExtensions
     {
-        /// <summary>Indicates whether or not the class has a specific interface.</summary>
-        /// <returns>Whether or not the SyntaxList contains the attribute.</returns>
-        public static bool HasInterface(this INamedTypeSymbol declaredTypeSymbol, GeneratorSyntaxContext context, string interfaceName)
-        {
-            var namedTypeSymbol = context.SemanticModel.Compilation.GetTypeByMetadataName(interfaceName);
-
-            foreach (var interfaceTypeSymbol in declaredTypeSymbol.AllInterfaces)
-            {
-                if (SymbolEqualityComparer.Default.Equals(interfaceTypeSymbol, namedTypeSymbol))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
         public static string ConvertToUnsecureString(this SecureString securePassword)
         {
             if (securePassword == null)
